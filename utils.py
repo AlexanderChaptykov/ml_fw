97251c1502aa5abc1b2cacf6b011c5fffb68dd63
Alexander Chaptykov <achaptykov@ubic.tech>
2019-11-20 13:17:53 +0300

    utils

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 def is_valid_txt(txt: str) -> bool:
     """get text return where True good text"""
-    if len(txt.split()) == 0 or pd.isnull(txt) or type(txt) != str:
+    if type(txt) != str:
+        return False
+    if len(txt.split()) == 0 or pd.isnull(txt):
         return False
     return True
