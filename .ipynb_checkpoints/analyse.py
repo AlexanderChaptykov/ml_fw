fd3b27a293705661a74627993aa3078201307272
Alexander Chaptykov <achaptykov@ubic.tech>
2019-12-02 13:16:38 +0300

    kmeans

---
+++ .ipynb_checkpoints/analyse.py
@@ -0,0 +1,20 @@
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+import numpy as np
+import matplotlib.pyplot as plt
+
+# k means determine k
+def kmeans(X, max_clusters=10):
+    distortions = []
+    K = range(1, max_clusters)
+    for k in K:
+        kmeanModel = KMeans(n_clusters=k).fit(X)
+        kmeanModel.fit(X)
+        distortions.append(sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+
+    # Plot the elbow
+    plt.plot(K, distortions, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Distortion')
+    plt.title('The Elbow Method showing the optimal k')
+    plt.show()
