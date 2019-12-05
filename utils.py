e2fe33eee8c20524dc55e6be7627a33f489c8f34
Alexander Chaptykov <achaptykov@ubic.tech>
2019-12-05 15:23:54 +0300

    addition

--- utils.py
+++ utils.py
@@ -26,4 +26,19 @@ def click_patch(self, req):
     return pd.DataFrame(self.execute(req), columns=cols)
 
 
+def obj_dic(d):
+    """Convert dict to object"""
+    top = type('new', (object,), d)
+    seqs = tuple, list, set, frozenset
+    for i, j in d.items():
+        if isinstance(j, dict):
+            setattr(top, i, obj_dic(j))
+        elif isinstance(j, seqs):
+            setattr(top, i,
+                type(j)(obj_dic(sj) if isinstance(sj, dict) else sj for sj in j))
+        else:
+            setattr(top, i, j)
+    return top
+
+
 Client.get_df = click_patch
